6e8662195862546d637e7ff5e2557b6a4d056e42
DavidBartek <davidwbartek@gmail.com>
2023-10-10 11:06:46 -0500

    user profile detail page displays all required data, closes #24

--- client/src/components/userprofiles/UserProfileDetails.js
+++ client/src/components/userprofiles/UserProfileDetails.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getUserById } from "../../managers/userProfileManager";
+import defaultCatImage from '../../assets/defaultcat.png';
 
 export default function UserProfileDetails() {
-  const [userProfile, setUserProfile] = useState();
+  const [userProfile, setUserProfile] = useState({});
 
   const { id } = useParams();
 
@@ -11,14 +12,35 @@ export default function UserProfileDetails() {
     getUserById(id).then(setUserProfile);
   }, [id]);
 
+  const convertDateTimeString = (dateTimeString) => {
+    const inputDate = new Date(dateTimeString);
+
+    const month = String(inputDate.getMonth() + 1).padStart(2, "0");
+    const day = String(inputDate.getDate()).padStart(2, "0");
+    const year = inputDate.getFullYear();
+
+    return `${month}/${day}/${year}`
+  }
+
+  const convertRolesToString = (rolesArray) => {
+    if (rolesArray && rolesArray.length > 0) {
+      return rolesArray.join(", ");
+    } else {
+      return "none specified"
+    }
+  }
+
   if (!userProfile) {
     return null;
   }
   return (
     <>
-      <img src={userProfile.imageLocation} alt={userProfile.firstName} />
+      <img src={userProfile.imageLocation} alt={defaultCatImage} />
       <h3>{userProfile.fullName}</h3>
-      <p>Username: {userProfile.userName}</p>
+      <p>Display name: {userProfile.userName}</p>
+      <p>Email: {userProfile.email}</p>
+      <p>Join date: {convertDateTimeString(userProfile.createDateTime)}</p>
+      <p>Profile type: {convertRolesToString(userProfile?.roles)}</p>
     </>
   );
 }
