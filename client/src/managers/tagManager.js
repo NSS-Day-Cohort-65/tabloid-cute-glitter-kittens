c8af58d551e948bf27b64a15353d56e099fc95bc
John Lu <johnlu2006@gmail.com>
2023-10-11 13:18:40 -0500

    add data access function

--- client/src/managers/tagManager.js
+++ client/src/managers/tagManager.js
@@ -3,3 +3,13 @@ const apiUrl = "/api/tag"
 export const getTags = () => {
     return fetch(apiUrl).then((res) => res.json());
 };
+
+export const createTag = (tag) => {
+    return fetch(apiUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(tag)
+    }).then((res) => res.json());
+};
