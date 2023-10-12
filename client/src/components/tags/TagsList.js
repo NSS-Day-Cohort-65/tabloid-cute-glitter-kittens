39dd0b30a4f7683008d52e7ee30c90fbbff7a7f3
John Lu <johnlu2006@gmail.com>
2023-10-12 10:58:51 -0500

    add the modal for delete confirmation

--- client/src/components/tags/TagsList.js
+++ client/src/components/tags/TagsList.js
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
 import { getTags } from "../../managers/tagManager";
-import { Button, Table } from "reactstrap";
+import { Table, Button, ModalHeader, ModalBody, Modal, ModalFooter } from "reactstrap";
 import { Link } from "react-router-dom"
 
 export default function TagsList() {
     const [tags, setTags] = useState([])
+    const [modal, setModal] = useState(false);
+    const [selectedTag, setSelectedTag] = useState([]);
 
     useEffect(() => {
         getTags().then(setTags)
     }, [])
 
-    const handleDelete = (id) => {
-        console.log(`deleting ${id}`)
+    const toggleModal = () => {setModal(!modal)};
+
+    const handleDelete = (tag) => {
+        toggleModal();
+        setSelectedTag(tag);
+        console.log(`set to ${tag.id}`)
     }
+
+const handleConfirmedDelete = () => {
+    console.log(`deleted ${selectedTag.name} `)
+    toggleModal()
+    //getTags.then(setTags)
+}
 
     return (
         <div className="container">
@@ -34,7 +46,7 @@ export default function TagsList() {
                         <tr key={t.id}>
                             <th scope="row">{`${t.name}`}</th>
                             <td>
-                                <Button onClick={() => {handleDelete(t.id)}}>
+                                <Button onClick={() => {handleDelete(t)}}>
                                     Delete Tag
                                 </Button>
                             </td>
@@ -43,6 +55,23 @@ export default function TagsList() {
                     ))}
                 </tbody>
             </Table>
+
+            <Modal isOpen={modal}>
+            <ModalHeader>Delete Tag</ModalHeader>
+
+            <ModalBody>
+            Are you sure you want to delete #{selectedTag.name} ?
+            </ModalBody>
+
+            <ModalFooter>
+            <Button color="warning" onClick={() => handleConfirmedDelete()}>
+                Ok
+            </Button>
+            <Button onClick={() => toggleModal()}>
+                No
+            </Button>
+            </ModalFooter>
+        </Modal>
 
         </div>
     )
